a066191bafa4abaca48042b1f4dfceb583716842
pvchine <pvchinbn@yahoo.com>
2021-02-07 16:14:08 +0800

    example 3 complete

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -3,6 +3,7 @@ const result = document.querySelector(".result");
 const fetchData = async () => {
   try {
     const { data } = await axios.get("/api/2-basic-api");
+
     const products = data
       .map((product) => {
         const {
